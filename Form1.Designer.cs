6be417e28f2ac5c239c099330b6d1c6e7a7c0425
Rasim Tuğberk İNCİ <38505417+tugberkinci@users.noreply.github.com>
2021-09-03 13:56:45 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.StratButton = new System.Windows.Forms.Button();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.ServerIPtextBox = new System.Windows.Forms.TextBox();
+            this.ServerPorttextBox = new System.Windows.Forms.TextBox();
+            this.ClientIPtextBox = new System.Windows.Forms.TextBox();
+            this.ClientPorttextBox = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SERVER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CLIENT";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "IP";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(276, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "PORT";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(102, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "IP";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(276, 127);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "PORT";
+            // 
+            // StratButton
+            // 
+            this.StratButton.Location = new System.Drawing.Point(464, 48);
+            this.StratButton.Name = "StratButton";
+            this.StratButton.Size = new System.Drawing.Size(75, 23);
+            this.StratButton.TabIndex = 6;
+            this.StratButton.Text = "START";
+            this.StratButton.UseVisualStyleBackColor = true;
+            this.StratButton.Click += new System.EventHandler(this.StratButton_Click);
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(464, 118);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 7;
+            this.ConnectButton.Text = "CONNECT";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // ServerIPtextBox
+            // 
+            this.ServerIPtextBox.Location = new System.Drawing.Point(140, 53);
+            this.ServerIPtextBox.Name = "ServerIPtextBox";
+            this.ServerIPtextBox.Size = new System.Drawing.Size(100, 20);
+            this.ServerIPtextBox.TabIndex = 8;
+            // 
+            // ServerPorttextBox
+            // 
+            this.ServerPorttextBox.Location = new System.Drawing.Point(328, 49);
+            this.ServerPorttextBox.Name = "ServerPorttextBox";
+            this.ServerPorttextBox.Size = new System.Drawing.Size(100, 20);
+            this.ServerPorttextBox.TabIndex = 9;
+            // 
+            // ClientIPtextBox
+            // 
+            this.ClientIPtextBox.Location = new System.Drawing.Point(140, 124);
+            this.ClientIPtextBox.Name = "ClientIPtextBox";
+            this.ClientIPtextBox.Size = new System.Drawing.Size(100, 20);
+            this.ClientIPtextBox.TabIndex = 10;
+            // 
+            // ClientPorttextBox
+            // 
+            this.ClientPorttextBox.Location = new System.Drawing.Point(328, 120);
+            this.ClientPorttextBox.Name = "ClientPorttextBox";
+            this.ClientPorttextBox.Size = new System.Drawing.Size(100, 20);
+            this.ClientPorttextBox.TabIndex = 11;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Black;
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(609, 28);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "label7";
+            this.label7.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Black;
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(609, 28);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "label8";
+            this.label8.Visible = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Black;
+            this.label9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(609, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "label9";
+            this.label9.Visible = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Black;
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(609, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "label10";
+            this.label10.Visible = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Black;
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(609, 48);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 19;
+            this.label11.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 197);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.ClientPorttextBox);
+            this.Controls.Add(this.ClientIPtextBox);
+            this.Controls.Add(this.ServerPorttextBox);
+            this.Controls.Add(this.ServerIPtextBox);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.StratButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button StratButton;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox ServerIPtextBox;
+        private System.Windows.Forms.TextBox ServerPorttextBox;
+        private System.Windows.Forms.TextBox ClientIPtextBox;
+        private System.Windows.Forms.TextBox ClientPorttextBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client { get; set; }
+        public StreamReader STR { get; set; }
+        public StreamWriter STW { get; set; }
+        public string recieve { get; set; }
+        public string TextToSend { get; set; }
+        public int horizontal { get; set; }
+        public int vertical { get; set; }
+        public bool ConnectButtonWasClicked { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            ConnectButtonWasClicked = false;
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach(IPAddress adress in localIP)
+            {
+                if (adress.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ServerIPtextBox.Text = adress.ToString();
+                }
+            }
+        }
+       
+
+        /// <summary>
+        /// Starter of server
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StratButton_Click(object sender, EventArgs e)
+        {
+             TcpListener listener = new TcpListener(IPAddress.Any, int.Parse(ServerPorttextBox.Text));
+            listener.Start();
+            client = listener.AcceptTcpClient();
+            STR = new StreamReader(client.GetStream());
+            STW = new StreamWriter(client.GetStream());
+            STW.AutoFlush = true;
+
+            backgroundWorker1.RunWorkerAsync();
+
+            this.label10.Visible = true;
+            this.label10.Text = "Im Server!";
+
+            VisibleOptions();
+            
+        }
+
+        /// <summary>
+        /// Connect for client side.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            
+            client = new TcpClient();
+            IPEndPoint IpEnd = new IPEndPoint(IPAddress.Parse(ClientIPtextBox.Text), int.Parse(ClientPorttextBox.Text));
+            try
+            {
+                client.Connect(IpEnd);
+                if(client.Connected)
+                {
+                    
+                    this.label7.Visible = true;
+                    this.label7.Text = "Connected to Server";
+
+                    STR = new StreamReader(client.GetStream());
+                    STW = new StreamWriter(client.GetStream());
+                    STW.AutoFlush = true;
+                    
+                    backgroundWorker1.RunWorkerAsync();
+                    this.label9.Visible = true;
+                    this.label9.Text = "Im Client!";
+                    ConnectButtonWasClicked = true;
+                    this.label11.Visible = true;
+                    
+
+                    VisibleOptions();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Set compenents are non visible after connection.
+        /// </summary>
+        private void VisibleOptions()
+        {
+            this.Size = new Size(730, 450);
+            this.BackColor = Color.Black;
+            this.ClientIPtextBox.Visible = false;
+            this.ClientPorttextBox.Visible = false;
+            this.ServerIPtextBox.Visible = false;
+            this.ServerPorttextBox.Visible = false;
+            this.label1.Visible = false;
+            this.label2.Visible = false;
+            this.label3.Visible = false;
+            this.label4.Visible = false;
+            this.label5.Visible = false;
+            this.label6.Visible = false;
+            this.StratButton.Visible = false;
+            this.ConnectButton.Visible = false;
+            
+
+        }
+
+     
+        /// <summary>
+        /// Listener method.Also this method draws rectangel on client screen.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            while (client.Connected)
+            {
+                Graphics dc = this.CreateGraphics();
+                Pen SalmonPen = new Pen(Color.DarkSalmon, 3);
+                Pen BlackPen = new Pen(Color.Black, 3);
+                dc.DrawRectangle(SalmonPen, horizontal, vertical, 100, 75);
+
+                try
+                {
+                    recieve = STR.ReadLine();
+                    this.label11.Invoke(new MethodInvoker(delegate ()
+                    {
+                        label11.Text = recieve;
+                    }));
+
+
+                    if (String.Equals(recieve, "Down"))
+                    {
+                        
+                        dc.DrawRectangle(BlackPen, horizontal, vertical, 100, 75);
+                        recieve = recieve + vertical++;
+                        
+                    }
+                    if (String.Equals(recieve, "Up"))
+                    {
+                       
+                        dc.DrawRectangle(BlackPen, horizontal, vertical, 100, 75);   
+                        recieve = recieve + vertical--;
+                        
+                    }                        
+                    if (String.Equals(recieve, "Left"))
+                    {
+                        
+                        dc.DrawRectangle(BlackPen, horizontal, vertical, 100, 75);
+                        recieve = recieve + horizontal--;
+                       
+                    }
+                        
+                    if (String.Equals(recieve, "Right"))
+                    {
+                        
+                        dc.DrawRectangle(BlackPen, horizontal, vertical, 100, 75);
+                        recieve = recieve + horizontal++;
+                        
+                    }
+                 
+
+
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// This method tells to client which arrow key pressed. Also it draws rectangle on server screen.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            if (client.Connected)
+            {
+
+                STW.WriteLine(TextToSend);
+                //RectangleDrawer(horizontal, vertical, TextToSend);
+
+                Graphics dc = this.CreateGraphics();
+                Pen SalmonPen = new Pen(Color.DarkSalmon, 3);
+                Pen BlackPen = new Pen(Color.Black, 3);
+                dc.DrawRectangle(BlackPen, horizontal, vertical, 100, 75);
+
+                if (String.Equals(TextToSend, "Down"))
+                {
+
+                    dc.DrawRectangle(BlackPen, horizontal, vertical++, 100, 75);
+                    dc.DrawRectangle(SalmonPen, horizontal, vertical, 100, 75);
+
+
+                }
+                if (String.Equals(TextToSend, "Up"))
+                {
+                    dc.DrawRectangle(BlackPen, horizontal, vertical--, 100, 75);
+                    dc.DrawRectangle(SalmonPen, horizontal, vertical, 100, 75);
+
+
+                }
+                if (String.Equals(TextToSend, "Left"))
+                {
+                    dc.DrawRectangle(BlackPen, horizontal--, vertical, 100, 75);
+                    dc.DrawRectangle(SalmonPen, horizontal, vertical, 100, 75);
+
+
+                }
+
+                if (String.Equals(TextToSend, "Right"))
+                {
+                    dc.DrawRectangle(BlackPen, horizontal++, vertical, 100, 75);
+                    dc.DrawRectangle(SalmonPen, horizontal, vertical, 100, 75);
+
+
+                }
+
+                this.label8.Invoke(new MethodInvoker(delegate ()
+                {
+                   label8.Text = TextToSend;
+                }));
+            }
+            else
+            {
+                MessageBox.Show("Sending failed");
+            }
+            //backgroundWorker2.CancelAsync();
+
+        }
+
+
+        /// <summary>
+        /// Gets information of which key pressed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(!ConnectButtonWasClicked)
+            { 
+            this.label8.Visible = true;
+            if (e.KeyCode == Keys.Down)
+            {
+                TextToSend = "Down";
+                    //20 ms delay for every key request
+                Task.Delay(20).ContinueWith(t => backgroundWorker2.RunWorkerAsync());
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                TextToSend = "Up";
+                Task.Delay(20).ContinueWith(t => backgroundWorker2.RunWorkerAsync());
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                TextToSend = "Left";
+                Task.Delay(20).ContinueWith(t => backgroundWorker2.RunWorkerAsync());
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                TextToSend = "Right";
+                Task.Delay(20).ContinueWith(t => backgroundWorker2.RunWorkerAsync());
+            }
+            }
+        }
+
+      
+    }
+}
